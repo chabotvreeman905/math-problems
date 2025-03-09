d16e3f8879dedd2c5c5c6a4011ce81f74bdfd997
chabotvreeman905 <chabotvreeman905@gmail.com>
2025-03-09 05:23:54 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+    double result = pow(n, 2);
+    cout << "The square of your number is: " << result << endl;
+    return 0;
+}
