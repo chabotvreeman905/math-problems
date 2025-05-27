ed7508037b644d4da4e5ca9fd0211f62c33965c9
chabotvreeman905 <chabotvreeman905@gmail.com>
2025-05-27 03:05:26 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "The number of sides in an equilateral triangle is: " << n + 1 << endl;
+    return 0;
+}
